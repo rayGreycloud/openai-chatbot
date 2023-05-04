e6ca89e28368de115bffd39f5e7ba0abae8dfe73
rayGreycloud <raygreycloud@gmail.com>
2023-05-04 12:59:19 -0700

    add message container

---
+++ client/src/components/ChatContainer/ChatContainer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ChatContainer = ({ messages }) => {
+  return (
+    <ul className='divide-y divide-gray-800'>
+      {messages.length > 1 &&
+        messages.map((message, idx) => {
+          if (idx === 0) return null;
+
+          return (
+            <li key={idx} className='flex justify-between gap-x-6 py-5'>
+              <div className='flex gap-x-4'>
+                <div className='text-sm font-semibold leading-6 text-white'>
+                  {message.role}
+                </div>
+                <div className='text-sm leading-6 text-white'>
+                  {message.content}
+                </div>
+              </div>
+            </li>
+          );
+        })}
+    </ul>
+  );
+};
+
+export default ChatContainer;
