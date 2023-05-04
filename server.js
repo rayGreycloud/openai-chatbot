9fd07c39de396064860606cef5df5caba0470421
rayGreycloud <raygreycloud@gmail.com>
2023-05-04 08:02:10 -0700

    add server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+
+const pkgJSON = require('./package.json');
+console.log(`APP: ${pkgJSON.name} v${pkgJSON.version}`);
+
+const app = express();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/chat', require('./routes/api/chat'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // All routes to build file
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// Set port for heroku or local
+const PORT = process.env.PORT || 5001;
+
+// Start server
+app.listen(PORT, (error) => {
+  if (error) throw error;
+  console.log(`SERVER: Listening on port ${PORT}`);
+});
