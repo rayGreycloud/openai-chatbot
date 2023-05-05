63f165f18f4121726f93ad8f74283f4030d5b0ce
rayGreycloud <raygreycloud@gmail.com>
2023-05-05 10:29:30 -0700

    display names

--- client/src/components/ChatContainer/ChatContainer.jsx
+++ client/src/components/ChatContainer/ChatContainer.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const ChatContainer = ({ messages }) => {
+const ChatContainer = ({ botName, messages }) => {
   return (
     <ul className='divide-y divide-gray-800'>
       {messages.length > 1 &&
         messages.map((message, idx) => {
           if (idx === 0) return null;
 
+          const role = message.role === 'user' ? 'You' : botName;
+
           return (
             <li key={idx} className='flex justify-between gap-x-6 py-5'>
               <div className='flex gap-x-4'>
                 <div className='text-sm font-semibold leading-6 text-white uppercase'>
-                  {message.role}:&nbsp;
+                  {role}:&nbsp;
                 </div>
                 <div className='text-sm leading-6 text-white'>
                   {message.content}
